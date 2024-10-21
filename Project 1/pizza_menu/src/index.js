0ca9b3fc163e0ca9bb4c1744cb26e20ee002f6b8
Saad Does Code <saad.does.code@gmail.com>
2024-10-21 17:40:49 +0500

    Added Stylings

--- Project 1/pizza_menu/src/index.js
+++ Project 1/pizza_menu/src/index.js
@@ -1,72 +1,114 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import './index.css';
 
 const pizzaData = [
-    {
-      name: "Focaccia",
-      ingredients: "Bread with italian olive oil and rosemary",
-      price: 6,
-      photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Margherita",
-      ingredients: "Tomato and mozarella",
-      price: 10,
-      photoName: "pizzas/margherita.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Spinaci",
-      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-      price: 12,
-      photoName: "pizzas/spinaci.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Funghi",
-      ingredients: "Tomato, mozarella, mushrooms, and onion",
-      price: 12,
-      photoName: "pizzas/funghi.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Salamino",
-      ingredients: "Tomato, mozarella, and pepperoni",
-      price: 15,
-      photoName: "pizzas/salamino.jpg",
-      soldOut: true,
-    },
-    {
-      name: "Pizza Prosciutto",
-      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-      price: 18,
-      photoName: "pizzas/prosciutto.jpg",
-      soldOut: false,
-    },
-  ];
-  
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-function App(){
-
-    return <>
-    <h1>Hello React!</h1>
-    <Pizza />
-    <Pizza />
-    <Pizza />
-    </>
-}
-
-function Pizza(){
-    return <div className="container">
-            <img height={"100px"} width={"100px"} src="pizzas/focaccia.jpg" alt="focaccia pizza" />
-            <h3>Focaccia</h3>
-            <p>Bread with italian olive oil and rosemary</p>
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
+  );
 }
 
-let root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<React.StrictMode>
-    <App/>
-</React.StrictMode>)
+function Header() {
+    // const style = {color:"red", fontSize:"8rem"};
+    const style = {}
+  return <header className="header">
+    <h1 style={style}>Fast React Pizza Co.</h1>
+  </header>;
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  console.log(hour)
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  return { isOpen } ? (
+    <footer className="footer">{new Date().toLocaleTimeString()}... We are open now!!!</footer>
+  ) : (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}... Sorry, We are closed!!!
+    </footer>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+function Pizza() {
+  return (
+    <div className="">
+      <img
+        // height={"100px"}
+        // width={"100px"}
+        src="pizzas/focaccia.jpg"
+        alt="focaccia pizza"
+      />
+      <h3>Focaccia</h3>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
